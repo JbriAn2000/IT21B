b3768cb8c03eba66b213ae79851065f9690362c5
JbriAn2000 <saliring0531@gmail.com>
2025-08-29 16:33:19 +0800

    Added subtraction, multiplication and division

--- hello.js
+++ hello.js
@@ -4,6 +4,12 @@ num1 = 2;
 num2 = 3;
 
 sum = num1 + num2;
+sub = num1 - num2;
+mul = num1 * num2;
+div = num1 / num2;
 
 console.log("Sum of " + num1 + " " + " and " + num2 + " " + " is " + sum);
+console.log("Subtraction of " + num1 + " " + " and " + num2 + " " + " is " + sub);
+console.log("Multiplication of " + num1 + " " + " and " + num2 + " " + " is " + mul);
+console.log("Division of " + num1 + " " + " and " + num2 + " " + " is " + div);
 
