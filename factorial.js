848a7152a57ee51c1d7923dce3df826f5d689d9c
JbriAn2000 <saliring0531@gmail.com>
2025-09-09 10:31:51 +0800

    Added computeFactorial() function and initial instance

--- factorial.js
+++ factorial.js
@@ -18,7 +18,7 @@ rl.question("Enter your choice (1-3): ", choice =>{
     if (choice === "1"){
         sayHello();
     } else if (choice === "2"){
-        console.log("Factorial");
+       computeFactorial();
         
          rl.question("Please enter a number for factorial: ", numStr =>{
             let num = parseInt(numStr);
@@ -44,5 +44,24 @@ rl.question("Enter your choice (1-3): ", choice =>{
 }
   function sayHello(){
     console.log("Hello");
+  }
+  function computeFactorial(){
+    console.log("Compute Factorial");
+    
+         rl.question("Please enter a number for factorial: ", numStr =>{
+            let num = parseInt(numStr);
+
+            if (isNaN(num) || num < 0){
+                console.log("Please enter a non-negative integer");
+            } else {
+                let fact = 1;
+
+                for (let i=1; i <= num; i++){
+                    fact *=i;
+                }
+
+                console.log("The factorial of " + num + " is " + fact);
+            }
+        })
   }
 mainMenu();
